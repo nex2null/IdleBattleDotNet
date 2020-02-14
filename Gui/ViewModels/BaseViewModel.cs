9f7b68027076751b7a320e91d3435e7fab66f78e
Brian <nex2null@gmail.com>
2020-02-13 21:52:47 -0500

    Cleaning up the WPF viewmodel boilerplate a bit.

---
+++ Gui/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Gui.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// INotifyPropertyChanged boilerplate
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void UpdateProperty(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
